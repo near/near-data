2edcd3c3288b05acce9750cdf47be60bad91efcd
Yifang Ma <yifang@nearprotocol.com>
2021-08-24 10:12:35 +0800

    set up build

--- src/App.js
+++ src/App.js
@@ -3,9 +3,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        NEAR DATA CENTER
-      </header>
+      <header className="App-header">NEAR DATA CENTRAL COMING SOON</header>
     </div>
   );
 }
